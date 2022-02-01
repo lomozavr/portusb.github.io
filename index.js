27d1003736a2e0671e5be508d4fed642a4da0a0d
lomozavr <98486101+lomozavr@users.noreply.github.com>
2022-02-01 21:50:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let btn = document.createElement("button");
-btn.innerHTML = "Click";
-btn.onclick = function () {
-  alert("Button is clicked");
-};
+btn.innerHTML = "OZON";
+btn.style['color'] = "white";
+btn.style['backgroundColor'] = "#005bff";
+btn.style['border'] = "none";
+btn.style['border-radius'] = '4px';
+btn.style['padding'] = '6px 12px';
+btn.onclick= function() {
+    location.href = "https://www.ozon.ru/"; }
 document.body.appendChild(btn)
