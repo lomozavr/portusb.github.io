0507856c8f07a18fa3ecee0a357ede686f1d5959
lomozavr <98486101+lomozavr@users.noreply.github.com>
2022-02-01 22:55:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ btn.style['backgroundColor'] = "#005bff";
 btn.style['border'] = "none";
 btn.style['border-radius'] = '4px';
 btn.style['padding'] = '6px 12px';
-btn.onclick= function() {
-    location.href = "https://www.ozon.ru/seller/port-usb-253731/products/?miniapp=seller_253731"; }
+btn.style['width'] = '60px';
+btn.style['height'] = '38px';
+btn.onclick = function() {
+  location.href = "https://www.ozon.ru/seller/port-usb-253731/products/?miniapp=seller_253731";
+}
 document.getElementById('tile-cover-8XGkwV').appendChild(btn);
