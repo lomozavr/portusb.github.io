636b6341a9e8b40568cc6ad7623dfe1177b27cbf
lomozavr <98486101+lomozavr@users.noreply.github.com>
2022-02-01 22:01:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let btn = document.createElement("button");
+buttons = document.getElementsByClassName("ins-tile__buttons");
 btn.innerHTML = "OZON";
 btn.style['color'] = "white";
 btn.style['backgroundColor'] = "#005bff";
@@ -6,5 +7,5 @@ btn.style['border'] = "none";
 btn.style['border-radius'] = '4px';
 btn.style['padding'] = '6px 12px';
 btn.onclick= function() {
-    location.href = "https://www.ozon.ru/seller/port-usb-253731/products/?miniapp=seller_253731/"; }
-document.body.appendChild(btn)
+    location.href = "https://www.ozon.ru/seller/port-usb-253731"; }
+buttons.appendChild(btn);
